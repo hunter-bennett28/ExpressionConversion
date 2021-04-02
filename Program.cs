977733ed0470fc92aa2e73e60c5e1053c1132a30
Hunter Bennett <hunter.ben28@hotmail.com>
2021-04-02 11:28:19 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Project2_Group_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
